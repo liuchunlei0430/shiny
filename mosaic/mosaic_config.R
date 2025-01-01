cd48c025bf2ad48ea3233902a68ecb30c9b6af07
Chunlei Liu <40068295+liuchunlei0430@users.noreply.github.com>
2025-01-01 21:10:35 +1100

    Add files via upload

---
+++ mosaic/mosaic_config.R
@@ -0,0 +1,48 @@
+
+library(reshape2)
+library(plyr)
+library(tidyverse)
+library(RColorBrewer)
+
+minmax <- function(x) {
+  if (max(x, na.rm = TRUE) == min(x, na.rm = TRUE)) {
+    return(rep(1, length(x)))
+  } else {
+    return((x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE)))
+  }
+}
+
+minmax2 <- function(x, new_min, new_max) {
+  scaled_x <- (x - new_min) / (new_max - new_min)  # 使用自定义范围进行缩放
+  return(scaled_x)
+}
+
+
+# Blues BuGn BuPu GnBu Greens Greys Oranges OrRd PuBu PuBuGn 
+# PuRd Purples RdPu Reds YlGn YlGnBu YlOrBr YlOrRd
+palettes <- list("overall" = "Reds",
+                 "batch.overall" = "Greens",
+                 "batch.metric" = "Greens",
+                 "clustering.overall" = "Blues",
+                 "clustering.metric" = "Blues",
+                 "classification.overall" = "PuRd",
+                 "classification.metric" = "PuRd",
+                 "classification_ori.overall" = "PuRd", #"RdPu",
+                 "classification_ori.metric" = "PuRd" #"RdPu"
+                 )
+
+text.metric <- c("method", "celltype")
+image.metric <- c("programmingLaguage", "DL", "Peak")
+overall.metric <- c("overall", "batch.overall", "clustering.overall", "classification.overall", "classification_ori.overall")
+
+clustering.metric <- c("ARI", "ASW", "cLISI", "iASW", "iFI", "NMI")
+classification.metric <- c("Average Accuracy", "Overall Accuracy", "f1_score", "sensitivity", "specificity")
+classification_ori.metric <- c("Average Accuracy_ori", "Overall Accuracy_ori", "f1_score_ori", "sensitivity_ori", "specificity_ori")
+batch.metric <- c("ASW_batch", "GC", "iLISI", "kBET", "NMI_batch", "ARI_batch")
+
+label_match <- data.frame(
+  label_new = c("Method", "Cell type", "Language", "DL", "Peak", "Overall score", "Clustering overall", "ARI", "ASW", "cLISI", "iFI", "iASW", "NMI", "Batch overall", "ASW", "GC", "iLISI", "kBET", 'NMI', "ARI", "Classification overall", "AAC", "OAC", "F1", "Sens", "Spec", "Seurat v3", "Seurat v5", "online iNMF"),
+  label_old = c("method", "celltype", "programmingLaguage", "DL", "Peak", "overall", "clustering.overall", "ARI", "ASW", "cLISI", "iFI", "iASW", "NMI", "batch.overall", "ASW_batch", "GC", "iLISI", "kBET", "NMI_batch", "ARI_batch", "classification.overall", "Average Accuracy", "Overall Accuracy", "f1_score", "sensitivity", "specificity", "Seurat.v3", "Seurat.v5", "online.iNMF"
+))
+
+
---
+++ time_speed/mosaic/mosaic_time_config.R
@@ -0,0 +1,22 @@
+library(glue)
+library(reshape2)
+library(ggplot2)
+library(ggpubr)
+
+
+scPalette <- function(n) {
+     colorSpace <- c('#E41A1C','#377EB8','#4DAF4A','#984EA3','#F29403','#F781BF','#BC9DCC','#A65628','#54B0E4','#222F75','#1B9E77','#B2DF8A',
+                     '#E3BE00','#FB9A99','#E7298A','#910241','#00CDD1','#A6CEE3','#CE1261','#5E4FA2','#8CA77B','#00441B','#DEDC00','#B3DE69',
+                     '#8DD3C7','#999999')
+    if (n <= length(colorSpace)) {
+        colors <- colorSpace[1:n]
+    } else {
+        colors <- grDevices::colorRampPalette(colorSpace)(n)
+    }
+    return(colors)
+}
+
+# 定义所有方法的列表
+all_methods <- c("Multigrate", "scMoMaT", "StabMap", "UINMF", "Cobolt", 
+                 "MultiVI", "SMILE")
+method_colors <- setNames(scPalette(length(all_methods)), all_methods)
